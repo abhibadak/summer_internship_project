e45a58c84fe4f3e75df6738a062f4c1526aa62b9
abhibadak <badakabhishek170@gmail.com>
2025-07-09 09:32:17 +0530

    Add files via upload

---
+++ mail.py
@@ -0,0 +1,19 @@
+import smtplib
+import streamlit as st
+
+From = "badakabhishek170@gmail.com"
+Sub = st.text_input("Enter subject:")
+To = st.text_input("Enter receiver's email:")
+msg =  st.text_input("Enter message:")
+
+text = f"Subject: {Sub}\n\n{msg}"
+
+if st.button("Send Email"):
+
+  with smtplib.SMTP("smtp.gmail.com",587) as server:
+    server.starttls()
+    server.login(From,password="vpzp wegb tmiy fodp")
+    server.sendmail(from_addr=From,to_addrs=To, msg=text)
+
+    print("Email sent successfully")
+    st.success("Email sent successfully")
